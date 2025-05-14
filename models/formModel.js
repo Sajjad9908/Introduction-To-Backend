55464f5e70e0d96098bf11cf7c1f8c93ea99f7e5
Sajjad9908 <gulbadan128@gmail.com>
2025-05-14 17:19:12 +0500

    again commit

--- models/formModel.js
+++ models/formModel.js
@@ -4,7 +4,7 @@ const formSchema= new mongoose.Schema({
     name:{required:true,type:String},
     FathersName:{required:true,type:String},
     gender:{required:true,type:String},
-    country:{required:true,type:String}
+    country:{required:true,type:String},
 })
 const UserForm=mongoose.model('UserForm',formSchema)
 module.exports=UserForm;
